b628fd53c323d00874e24306db0c39fbae1c2ba6
Casey Echternacht <casechtern@yahoo.com>
2024-07-24 22:01:46 -0600

    tested .add and .addAll methods

--- src/main/java/org/example/ExampleOne.java
+++ src/main/java/org/example/ExampleOne.java
@@ -1,5 +1,18 @@
 package org.example;
 
+import java.util.HashSet;
+
 public class ExampleOne {
-	//test comment
+	public static void main(String[] args) {
+		HashSet<Integer> evenNumbers = new HashSet<>();
+		evenNumbers.add(2);
+		evenNumbers.add(4);
+		evenNumbers.add(6);
+		System.out.println("HashSet: " + evenNumbers);
+
+		HashSet<Integer> numbers = new HashSet<>();
+		numbers.addAll(evenNumbers);
+		numbers.add(5);
+		System.out.println("New hashSet: " + numbers);
+	}
 }
